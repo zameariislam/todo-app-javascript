c9e4137b12319961f3a0bb4824f5ff1b8091e940
zameariislam <zameariislam@gmail.com>
2023-02-01 18:46:33 +0100

    input validation added

--- app.js
+++ app.js
@@ -6,34 +6,93 @@ function $(id) {
 
 
 }
+
+// selection 
 const form = $('form')
-const today = new Date().toISOString().slice(0, 10)
 const date = $('date')
+const tbody = $('tbody')
+
+const today = new Date().toISOString().slice(0, 10)
+
 date.value = today
 
 
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-   
+
     let inputElements = this.elements
     let data = {}
+    let isValid=true
 
 
     for (let input of inputElements) {
+
         if (input.type !== 'submit') {
-            data[input.name] = input.value
+            if (input.value === '') {
+                alert('please fill up all field ')
+                isValid=false
+                return
+
+            }
+            data[input.name]=input.value
+        
         }
 
-    } 
-    data.status='incomplete'
- 
+    }
+    if(isValid){
+        data.status='incomplete'
+        data.id="AA"+Date.now()
+
+        showUI(data)
+
+    }
+    
+    this.reset()
 
 
-
+   
 
 
 
 
 })
+
+
+const showUI = (data) => {
+    console.log(data)
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+
+    <tr >
+        <td>0</td>
+        <td>${data.name}</td>
+        <td> ${data.priority}</td>
+        <td>
+        ${data.status}
+        </td>
+        <td>${data.date}</td>
+        <td>
+
+            <button id="delete">
+                <i class="fas fa-trash-can"></i>
+            </button>
+            <button id="check">
+                <i class="fas fa-check"></i>
+            </button>
+            <button id="edit">
+                <i class="fa-regular fa-pen-to-square"></i>
+            </button>
+
+
+        </td>
+    </tr>
+    `
+    tbody.appendChild(tr)
+
+
+
+
+
+}
 
